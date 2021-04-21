d7f0ef96f65c425af373d370d34b5bde9c9e2d7f
RawanAldamaeen <rawan.aldemaeen@ibtikar.net.sa>
2021-04-21 13:32:11 +0300

    Modify .gitignore

--- reservation/settings.py
+++ reservation/settings.py
@@ -160,5 +160,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rawansd7@gmail.com'
-EMAIL_HOST_PASSWORD = 'vmnebjuosglvxmrw'
+EMAIL_HOST_USER = '*******'
+EMAIL_HOST_PASSWORD = '*****'
